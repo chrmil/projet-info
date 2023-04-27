6990b0bfab0d0599b006d2b0500be70d70e4a436
chrmil <130459909+chrmil@users.noreply.github.com>
2023-04-27 14:15:19 +0200

    Update case.c

--- case.c
+++ case.c
@@ -19,69 +19,70 @@ typedef struct {
 	int statut; //0 si case cachée, 1 sinon
 }Case;
 
-int count(int a, int* compteur){
+int count(int a, int* compteur){//gère le compteur et renvoie 0 si le nombre max de cases d'un ceratin type est déjà atteint et 1 sinon
   switch(a){
-	      case 1: //monstres
-				case 2:
-				case 3:
-				case 4:
-          if(compteur[0]>=16){
-              return 0;
-          }
-		      compteur[0]++;
-          return 1;
-				break;
-				case 5:
-				  if (compteur[1]<2){
-				  	compteur[1]++;
-            return 1;
-				  }
-			   	return 0;
-        break;
-				case 6:
-				  if (compteur[2]<2){
-					  compteur[2]++;
-            return 1;
-				  }
-  			  return 0;
-				break;
-				case 7:
-				  if (compteur[3]==0){
-					   compteur[3]++;
-             return 1;
-				  }
-  				return 0;
-				break;
-				case 8:
-				  if (compteur[4]==0){
-  					compteur[4]++;
-            return 1;
-				  }
-				  return 0;
-				break;
-				case 9:
-				  if (compteur[5]==0){
-					  compteur[5]++;
-            return 1;
-				  }
-				  return 0;
-				break;
-				case 10:
-				  if (compteur[6]==0){
-					  compteur[6]++;
-            return 1;
-				  }
-				  return 0;
-				break;
-				case 11:
-				  if (compteur[7]==0){
-					  compteur[7]++;
-            return 1;
-				  }
-				  return 0;
-          break;
-          default: return 0;
-		}
+	        case 1: //monstres
+		case 2:
+		case 3:
+		case 4:
+        		if(compteur[0]>=16){
+            	  		return 0;
+        	  	}
+			compteur[0]++;
+			return 1;
+		break;
+		case 5:
+			if (compteur[1]<2){
+				compteur[1]++;
+            			return 1;
+			}
+			return 0;
+        	break;
+		case 6:
+			if (compteur[2]<2){
+				compteur[2]++;
+            			return 1;
+			}
+  			return 0;
+		break;
+		case 7:
+			if (compteur[3]==0){
+				compteur[3]++;
+             			return 1;
+			}
+  			return 0;
+		break;
+		case 8:
+			if (compteur[4]==0){
+  				compteur[4]++;
+            			return 1;
+			}
+			return 0;
+		break;
+		case 9:
+			if (compteur[5]==0){
+				compteur[5]++;
+            			return 1;
+			}
+			return 0;
+		break;
+		case 10:
+			if (compteur[6]==0){
+				compteur[6]++;
+            			return 1;
+			}
+			return 0;
+		break;
+		case 11:
+			if (compteur[7]==0){
+				compteur[7]++;
+            			return 1;
+			}
+			return 0;
+          	break;
+          	default: 
+			return 0;
+	}
 }
 
 
@@ -117,19 +118,19 @@ void generateMap(Case map[][ARRAY]){
 	for (i=1; i<6; i++){
 		for(k=1;k<6;k++){
 			map[i][k].statut=0;//cases face cachée
-      a=rand()%11+1;
+      			a=rand()%11+1;
 			map[i][k].type=a; //type de la case
 			//compte le nb de cases d'un type sur le plateau; vérifie qu'il y en ait le bon nb ;
-	      while(count(a,compteur)==0) {
-              a=rand()%11+1;
-              map[i][k].type=a;
-        }
-    }
-  }
- for(i=0;i<8;i++){
-     printf("\nCompteur n°%d est à %d.",i,compteur[i]);
- }
-printf("\n");
+	      		while(count(a,compteur)==0) {
+              			a=rand()%11+1;
+              			map[i][k].type=a;
+        		}
+    		}
+  	}
+ 	for(i=0;i<8;i++){
+     		printf("\nCompteur n°%d est à %d.",i,compteur[i]);
+ 	}
+	printf("\n");
 }
 
 void viewMap(Case map[][ARRAY]){
@@ -138,7 +139,7 @@ void viewMap(Case map[][ARRAY]){
 	for (i=0; i<7; i++){
 	printf("\n");
 		for(k=0;k<7;k++){
-			printf(" %d (%d)",map[i][k].type, map[i][k].statut);
+			printf(" %d (%d)",map[i][k].type, map[i][k].statut);//affiche "nature de la case(statut de la case)" pour toute la map + les bords
 
 		}
 	}
@@ -146,8 +147,8 @@ void viewMap(Case map[][ARRAY]){
 }
 
 int main (){
-  Case map[ARRAY][ARRAY];
-  generateMap(map);//tab de 7*7 cases
-  viewMap(map);
-  return 0;
+ 	Case map[ARRAY][ARRAY];
+ 	generateMap(map);//tab de 7*7 cases
+ 	viewMap(map);
+ 	return 0;
 }
