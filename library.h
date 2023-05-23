6f9546b6fb00c71f8e83ef6bf355cbb1e38ee525
chrmil <130459909+chrmil@users.noreply.github.com>
2023-05-23 16:13:20 +0200

    Update library.h

--- library.h
+++ library.h
@@ -35,6 +35,9 @@ typedef struct Characterstats{
 	int artifact;		// 0 si arme antique pas trouvée, 1 sinon
 	int chest;		// nombre de coffres récupérés: 0, 1 ou 2
 	int pts;		// amount of points of a player
+	int treasure;
+	int monsters;
+	int exploration;
 	char name[10];		// 10 characters name
 
 }Character;			// player stats (Christelle)
