1512def198366a1501595d4f22e6800a56b88c0b
chrmil <130459909+chrmil@users.noreply.github.com>
2023-05-26 14:36:31 +0200

    Update menu.c

--- menu.c
+++ menu.c
@@ -16,11 +16,6 @@ char sortTab(Character tab[], char size){					// sorted tab verification
 	return 1;
 }
 
-long getTimeMs(){
-	struct timeval tv;
-	gettimeofday(&tv,NULL);
-	return (tv.tv_sec*1000)+(tv.tv_usec/1000);
-}
 
 void merge(Character tab[], char begin, char middle, char end, char size){	// mergesort Christelle
 	char i,k,j;
